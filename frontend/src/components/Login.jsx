9f2cd676d179cd11ed1a675d28745f5c1be20530
Syed Faraz Ahmad <farazahmadsfa@gmail.com>
2025-03-24 14:42:08 +0530

    port change

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -27,7 +27,7 @@ function Login() {
             return handleError("Email & Password are Required")
         }
         try{
-            const url = 'http://localhost:8000/auth/login'
+            const url = 'http://localhost:3000/auth/login'
             const response = await fetch(url,{
                 method:"POST",
                 headers:{
